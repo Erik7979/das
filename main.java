5f354de6c8d3744a2a25f43ffc9d296047f22c77
Erik7979 <you@example.com>
2021-04-22 17:36:44 -0700

    java

---
+++ main.java
@@ -0,0 +1,67 @@
+// Basic java program 
+// to print "hello World"
+
+
+import java.io.*;
+
+class main{
+	public static void main(String[] args)
+	{
+		// prints Hello World
+		/*int x = 5;
+		char symbol = 'a';
+		float f = 1.2f;
+		double d = 1.3;
+		boolean t = (5 > 4);*/
+		
+		double x = 15.0;
+                int b = 4;
+		
+		if (x > b) {
+		        // fjkgbsuhdr
+
+		} else {
+
+			// sssss
+		}
+
+		x = x + 5;
+		x += 5;
+	        
+		int c = 7;
+                x = c++;
+	        System.out.println(c);
+	        System.out.println(x);
+		int k = 9;
+		b = ++k;
+		
+		System.out.println(k);
+		System.out.println(b);
+
+
+		int a = 3;		
+
+		for ( a = 1; a < 21; ++a )
+		
+		System.out.println(a);
+
+
+
+		int y = 5;
+                
+		if ( y % 2 != 1)
+
+	        System.out.println("this number is even");
+
+		else 
+		System.out.println("this number is odd"); 
+
+
+		int w = 10;
+		if (w % 2 == 0)
+		System.out.println(w);	
+	}       
+
+			
+
+} 
